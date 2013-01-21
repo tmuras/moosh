04e751d3dce04daa96e23b88056f4fd54e303d34
Tomasz Muras <nexor1984@gmail.com>
2013-01-21 22:50:04 +0100

    Add lang strings extraction.

--- commands/dev/GenerateLang.php
+++ commands/dev/GenerateLang.php
@@ -11,22 +11,69 @@ class GenerateLang extends MooshCommand
     public function __construct()
     {
         parent::__construct('lang', 'generate');
+        $this->maxArguments = 255;
     }
 
     public function execute()
     {
+         $targets = array();
         //scan all files provided in as arguments or current directory for "."
         if(count($this->arguments)>0) {
             $targets = $this->arguments;
         } else { //or use the current edit file
+            $this->loadSession();
 
+            if(isset($this->session['generator.last-file'][$this->cwd]) && file_exists($this->cwd . '/' .$this->session['generator.last-file'][$this->cwd])) {
+                $targets = array($this->session['generator.last-file'][$this->cwd]);
+            }
         }
 
+        if(count($targets) == 0) {
+            cli_error('I don\'t know which file(s) to operate on');
+        }
 
+        $strings = array();
+        foreach($targets as $target) {
+            $strings[$target] = array();
+            $content = file_get_contents($this->cwd . '/' . $target);
+            $matches = null;
+            preg_match_all("/get_string\('([^']+)'([^)]*?)\)/",$content, $matches);
+            foreach($matches[1] as $k=>$lang) {
+                $key = $lang;
+                $langCat = null;
+                $matches2 = null;
+                if(preg_match("/'(.*)'/",$matches[2][$k],$matches2)) {
+                    //var_dump($matches[2][$k]);
+                    $langCat = $matches2[1];
+                    $key .= '|' . $langCat;
+                }
+                //echo "$key\n";
+                if(!isset($strings[$target][$key])) {
+                    $strings[$target][$key] = array(
+                        'number' => 1,
+                        'lang' => $lang,
+                        'category' => $langCat,
+                    );
+                } else {
+                    $strings[$target][$key]['number']++;
+                }
+            }
+        }
+
+        $langCategory = $this->getLangCategory();
+        $langKeys = array();
         //find all possible strings from the current mod's namespace
+        foreach($strings as $file=>$langFile) {
+            echo "File $file\n";
+            foreach($langFile as $langKey => $lang) {
+                echo "\t" .  $lang['lang'] .','. $lang['category'] .','. $lang['number'] . "\n";
+                if($langCategory == $lang['category']) {
+                    $langKeys[$lang['lang']] = $lang['lang'];
+                }
+            }
+        }
 
         //find lang file
-        $langCategory = $this->getLangCategory();
         $langFile = $this->topDir . '/' . $this->relativeDir . "/lang/en/$langCategory.php";
         if (!file_exists($langFile)) {
             //search a bit more?
@@ -44,16 +91,18 @@ class GenerateLang extends MooshCommand
             $parser = mlang_parser_factory::get_parser('php');
             $component = new mlang_component($this->pluginInfo['name'], 'en', mlang_version::by_branch('MOODLE_24_STABLE'));
             $parser->parse(file_get_contents($langFile), $component);
-            var_dump($component);
+            //check for the new lang strings
+            echo "Lang keys to be added to $langFile:\n";
+            foreach($langKeys as $key) {
+                if(!$component->has_string($key)) {
+                    echo "\t$key\n";
+                    $string = new \mlang_string($key,$key);
+                    $component->add_string($string);
+                }
+            }
+
+            //add them into the lang file
+            $component->export_phpfile($langFile);
         }
-
-        //add them into the lang file
-
-        //add string to a lang file: discovered or found in lang/en/*.php
-        var_dump($this->pluginInfo);
-        var_dump($this->relativeDir);
-        var_dump($this->topDir);
-        //var_dump($langFile);
-
     }
 }
