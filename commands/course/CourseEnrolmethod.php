d923a13afbe40eba852dca93d28e879e2afa6152
miotto <dropsy@gmail.com>
2013-03-08 17:10:19 +0100

    test new command

--- commands/course/CourseEnrolmethod.php
+++ commands/course/CourseEnrolmethod.php
@@ -15,7 +15,8 @@ class CourseEnrolmethod extends MooshCommand
     {
         parent::__construct('enrolmethod', 'course');
         $this->addOption('i|id', 'use numeric IDs instead of user name(s)');
-        $this->addOption('r|enrole:', 'enrole name');
+        $this->addOption('r|enrole:', 'enrole method name');
+        $this->addOption('m|maxenrol:', 'max enrolled user');
 
         //possible other options
         //duration
@@ -23,14 +24,15 @@ class CourseEnrolmethod extends MooshCommand
         //recovergrades
 
         $this->addArgument('courseid');
-        $this->addArgument('enrolm');
+        $this->addArgument('enrolmethod');
+        $this->addArgument('maxenrol');
         $this->maxArguments = 255;
     }
 
     public function execute()
     {
 
-        echo "aljadjad" . "\n";
+        echo "asdf" . "\n";
         global $CFG, $DB, $PAGE;
 
         require_once($CFG->dirroot . '/enrol/locallib.php');
@@ -39,27 +41,40 @@ class CourseEnrolmethod extends MooshCommand
         $options = $this->expandedOptions;
         $arguments = $this->arguments;
 
+
         //find role id for given role
 //        $role = $DB->get_record('role', array('shortname' => $options['role']), '*', MUST_EXIST);
 
-        $course = $DB->get_record('course', array('id' => $arguments[0]), '*', MUST_EXIST);
-        $context = get_context_instance(CONTEXT_COURSE, $course->id, MUST_EXIST);
-        $manager = new course_enrolment_manager($PAGE, $course);
+        //$course = $DB->get_record('course', array('id' => $arguments[0]), '*', MUST_EXIST);
+        //$context = get_context_instance(CONTEXT_COURSE, $course->id, MUST_EXIST);
+        //$manager = new course_enrolment_manager($PAGE, $course);
 
-        $instances = $manager->get_enrolment_instances();
+        //$instances = $manager->get_enrolment_instances();
         //find the manual one
-        foreach ($instances as $instance) {
-            //print_r("cvb");
-            echo $instance->enrol . "\n";
-        }
+        //foreach ($instances as $instance) {
+        //    //print_r("cvb");
+        //    echo $instance->enrol . "\n";
+        //}
 
 
-        //$maninstance1 = $DB->get_record('enrol', array('courseid'=>$course1->id, 'enrol'=>'manual'), '*', MUST_EXIST);
-        $instance1 = $DB->get_record('enrol', array('courseid'=>$course->id, 'enrol'=>'self'), '*', MUST_EXIST);
+         //print_r(array('maxenrol' => $arguments[2]));
+         //var_dump((string)($arguments[1]));
+        
+//$maninstance1 = $DB->get_record('enrol', array('courseid'=>$course1->id, 'enrol'=>'manual'), '*', MUST_EXIST);
+        //$instance1 = $DB->get_record('enrol', array('courseid'=>$course->id, 'enrol'=>'waitlist'), '*', MUST_EXIST);
+        $instance1 = $DB->get_record('enrol', array('courseid'=>$arguments[0], 'enrol'=>(string)$arguments[1]), '*', MUST_EXIST);
         //$instance1->expirythreshold = 60*60*24*4;
         //$instance1->expirynotify    = 1;
         //$instance1->notifyall       = 1;
-        $instance1->status          = ENROL_INSTANCE_ENABLED;
+        $curTZ     = new DateTimeZone('Europe/Berlin');
+        $startTime = date_create('2013-03-11 10:00:00', $curTZ);
+        $instance1->enrolstartdate = strtotime(date_format($startTime, 'Y-m-d H:i:s'));
+        //$instance1->enrolenddate   = strtotime("2013-03-22");
+        $endTime = date_create('2013-04-15 12:00:00', $curTZ);
+        $instance1->enrolenddate = strtotime(date_format($endTime, 'Y-m-d H:i:s'));
+	//maxenrolled
+//        $instance1->customint3     = $arguments[2];
+//        $instance1->status         = ENROL_INSTANCE_ENABLED;
         $DB->update_record('enrol', $instance1);
 
 
