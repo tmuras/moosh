540bd943ed09853678b12fa88a9698f5d5c1fa3b
Brian King <brian@liip.ch>
2013-06-08 14:41:55 +0200

    SqlDump: add postgresql support

--- Moosh/Command/Moodle23/Sql/SqlDump.php
+++ Moosh/Command/Moodle23/Sql/SqlDump.php
@@ -9,6 +9,7 @@
 namespace Moosh\Command\Moodle23\Sql;
 use Moosh\MooshCommand;
 
+//TODO: add compression option (mysql: (g)zip, postgres: -C option)
 class SqlDump extends MooshCommand
 {
     public function __construct()
@@ -20,10 +21,21 @@ class SqlDump extends MooshCommand
     {
         global $CFG, $DB;
 
-        if($CFG->dbtype != 'mysqli') {
-          cli_error('Only MySQL is currently supported');
+        switch ($CFG->dbtype) {
+            case 'mysqli':
+                passthru("mysqldump -h {$CFG->dbhost} -u {$CFG->dbuser} -p{$CFG->dbpass} {$CFG->dbname}");
+                break;
+            case 'pgsql':
+                $portoption = '';
+                if (!empty($CFG->dboptions['dbport'])) {
+                    $portoption = '-p ' . $CFG->dboptions['dbport'];
+                }
+                putenv('PGPASSWORD='.$CFG->dbpass);
+                passthru("pg_dump -h $CFG->dbhost -U $CFG->dbuser $portoption $CFG->dbname");
+                break;
+            default:
+                cli_error("Sorry, database type '$CFG->dbtype' is not supported yet.  Feel free to contribute!");
+                break;
         }
-
-        passthru("mysqldump -h {$CFG->dbhost} -u {$CFG->dbuser} -p{$CFG->dbpass} {$CFG->dbname}");
     }
 }
