06368840f45a15c01a6a18169dc04a16d3bbe83a
Brian King <brian@liip.ch>
2013-06-08 14:05:44 +0200

    File commands namespaced

--- Moosh/Command/Moodle23/File/FileList.php
+++ Moosh/Command/Moodle23/File/FileList.php
@@ -6,6 +6,9 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+namespace Moosh\Command\Moodle23\File;
+use Moosh\MooshCommand;
+
 class FileList extends MooshCommand
 {
     public function __construct()
