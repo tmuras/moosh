47edb0b6d3dd5516cf1b3c75495f0257da7d1e2a
mosa <mosarg@gmail.com>
2013-08-02 10:04:27 +0200

    user-delete step1: get user id

--- Moosh/Command/Moodle23/User/UserDelete.php
+++ Moosh/Command/Moodle23/User/UserDelete.php
@@ -14,24 +14,41 @@ class UserDelete extends MooshCommand
     public function __construct()
     {
         parent::__construct('delete', 'user');
-
-        //$this->addArgument('name');
-
-        //$this->addOption('t|test', 'option with no value');
-        //$this->addOption('o|option:', 'option with value and default', 'default');
+      
+        $this->addArgument('username');
 
     }
 
     public function execute()
     {
-        //some variables you may want to use
+        global $CFG, $DB;
+
+        require_once $CFG->dirroot . '/user/lib.php';
+        
+         foreach ($this->arguments as $argument) {
+             
+             $this->expandOptionsManually(array($argument));
+             $options = $this->expandedOptions;
+             $user = new \stdClass();
+                       
+             try {
+                $user = $DB->get_record('user', array('username' => $argument));
+                echo $user->id."\n";
+            } catch (Exception $e) {
+                print get_class($e)." thrown within the exception handler. Message: ".$e->getMessage()." on line ".$e->getLine();
+            }
+             
+         }
+         
+        
+//some variables you may want to use
         //$this->cwd - the directory where moosh command was executed
         //$this->mooshDir - moosh installation directory
         //$this->expandedOptions - commandline provided options, merged with defaults
         //$this->topDir - top Moodle directory
         //$this->arguments[0] - first argument passed
 
-        $options = $this->expandedOptions;
+        #$options = $this->expandedOptions;
 
         /* if verbose mode was requested, show some more information/debug messages
         if($this->verbose) {
