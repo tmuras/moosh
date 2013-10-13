82d1a580e2504b9d208161cd0f8ff191000a6af1
Tomasz Muras <nexor1984@gmail.com>
2013-10-13 23:45:45 +0200

    Show path to inside the module

--- Moosh/Command/Moodle23/Info/Info.php
+++ Moosh/Command/Moodle23/Info/Info.php
@@ -20,6 +20,6 @@ class Info extends MooshCommand
     {
         echo "Plugin type: ".$this->pluginInfo['type'] . "\n";
         echo "Plugin name: ".$this->pluginInfo['name'] . "\n";
-        echo "Plugin directory: ".$this->pluginInfo['dir'] . "\n";
+        echo "Plugin directory: ".$this->pluginInfo['dir'] . '/' . $this->pluginInfo['name'] . "\n";
     }
 }
