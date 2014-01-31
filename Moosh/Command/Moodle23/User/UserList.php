24a36578c5fa1b555a1b148d44feb5c378e0d85b
Daniel Kosinski <de.kosinski@gmail.com>
2014-01-31 15:28:20 +0100

    Make user-list accept sql WHERE clause

--- Moosh/Command/Moodle23/User/UserList.php
+++ Moosh/Command/Moodle23/User/UserList.php
@@ -19,11 +19,11 @@ class UserList extends MooshCommand
         parent::__construct('list', 'user');
 
         $this->addOption('n|limit:', 'display max n users');
-        $this->addOption('i|idnumber', 'display idnumber column');
+        $this->addOption('i|id', 'display id only column');
         $this->addOption('s|sort:', 'sort by (id, username, email or idnumber)');
         $this->addOption('d|descending', 'sort in descending order');
-        $this->addOption('f|fname:','first name');
-        $this->addOption('l|lname:','last name');
+
+        $this->addArgument('sql expression');
     }
 
     public function execute()
@@ -32,35 +32,32 @@ class UserList extends MooshCommand
 
         $options = $this->expandedOptions;
 
+        $users = ("SELECT * FROM {user} WHERE " . $this->arguments[0]);
+
         $sort = "id";
         if($options['sort'] == 'id' || $options['sort'] == 'username' || $options['sort'] == 'email' || $options['sort'] == 'idnumber'){
             $sort = $options['sort'];
         }
+
         $dir = 'ASC';
         if($options['descending']){
             $dir = 'DESC';
         }
 
-	if($options['fname'] and $options['lname']) {
-            try {
-                $user = $DB->get_record('user', array('firstname'=>$options['fname'], 'lastname'=>$options['lname']),'*', MUST_EXIST); 
-                echo $options['fname'] . " " . $options['lname'] . " id number is " . $user->id ."\n";
-            } catch (Exception $e) {
-                print get_class($e)." thrown within the exception handler. Message: ".$e->getMessage()." on line ".$e->getLine();
-            }
-        }
-
-        $select_sql = "SELECT * FROM {user} WHERE confirmed = 1 AND deleted = 0 ORDER BY $sort $dir";
+        $users .= " ORDER BY $sort $dir";
+ 
         if($options['limit'] && preg_match('/^\d+$/', $options['limit'])){
-            $select_sql .= " LIMIT " . $options['limit'];
+            $users .= " LIMIT " . $options['limit'];
         }
-        $users = $DB->get_records_sql($select_sql);
 
-        foreach($users as $user) {
-            $to_print = $user->username . " ({$user->id}), " . $user->email . ", ";
-            if($options['idnumber']){
-                $to_print .= $user->idnumber . ", ";
+        $users = $DB->get_records_sql($users);
+
+        foreach ($users as $user) {
+            if($options['id']) {
+                echo "$user->id \n";
+                continue;
             }
+            $to_print = $user->username . " ({$user->id}), " . $user->email . ", ";
             echo $to_print . "\n";
         }
     }
