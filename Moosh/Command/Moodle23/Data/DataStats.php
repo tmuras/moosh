a22c903ace546382fbd81866c9d9af3e32664ec6
Daniel Kosinski <de.kosinski@gmail.com>
2014-02-12 16:46:00 +0100

    If using json don't output anything else

--- Moosh/Command/Moodle23/Data/DataStats.php
+++ Moosh/Command/Moodle23/Data/DataStats.php
@@ -31,30 +31,31 @@ class DataStats extends MooshCommand
         }
         $pattern = '/\d*/';
         preg_match($pattern, $dataroot[0], $matches);
-        echo $matches[0] . "\n";
 
         exec("du -s $CFG->dataroot/filedir", $filedir, $return_value);
         if ($return_value != 0) {
             exit($return_value);
         }
         preg_match($pattern, $filedir[0], $dir_matches);
-        echo $dir_matches[0] . "\n";
 
         $sql_query = "SELECT SUM(filesize) AS total FROM {files}";
         $all_files = $DB->get_record_sql($sql_query);
-        echo $all_files->total . "\n";
 
         $sql_query = "SELECT DISTINCT contenthash, SUM(filesize) AS total FROM {files}";
         $distinct_contenthash = $DB->get_record_sql($sql_query);
-        echo $distinct_contenthash->total . "\n";
 
         if ($options['json']) {
-            $encode = array('dataroot' => $matches[0],
+            $to_encode = array('dataroot' => $matches[0],
                             'filedir' => $dir_matches[0], 
                             'files total' => $all_files->total, 
                             'distinct files total' => $distinct_contenthash->total);
 
-            return json_encode($encode);
+            return json_encode($to_encode);
+        } else {
+            echo $matches[0] . "\n";
+            echo $dir_matches[0] . "\n";
+            echo $all_files->total . "\n";
+            echo $distinct_contenthash->total . "\n";
         }
     }
 }
