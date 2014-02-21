46f95924d4e496afdf30f05f9709d760dc2bf6fc
Tomasz Muras <nexor1984@gmail.com>
2014-02-21 21:41:06 +0100

    Few extra checks

--- tests/functions.sh
+++ tests/functions.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 source config.sh
 
+if [ -z "${DBNAME}" ]; then
+    echo "Moodle DB is not set up in \$DBNAME"
+    exit 1
+fi
+
+if [[ ! $(echo select 1 | mysql -u $DBUSER -p$DBPASSWORD $DBNAME) ]]; then
+    echo Could not connect to the database, check \$DBUSER, \$DBNAME and \$DBPASSWORD
+fi
+
+if [ -z "${MOODLEDIR}" ]; then
+    echo "Moodle directory \$MOODLEDIR is not set"
+    exit 1
+fi
+
+if [ -z "${MOODLEDATA}" ]; then
+    echo "Moodle data directory \$MOODLEDATA is not set"
+    exit 1
+fi
+
+
+exit 0
 function install_db {
   cd ../data
   bzip2 -dk moodle.sql.bz2
