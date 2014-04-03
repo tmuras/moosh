1fff68623f3a93ec3318e396d997bf275d8b90a1
Daniel Kosinski <de.kosinski@gmail.com>
2014-04-03 16:34:10 +0200

    Add test for debug functions

---
+++ tests/debug-onoff.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+source functions.sh
+
+#install_db
+#install_data
+
+cd $MOODLEDIR
+
+moodle_url=$(echo $MOODLEDIR | grep -oP "[^\/]+$")
+
+moosh debug-on
+if curl http://127.0.0.1/$moodle_url/ | grep "core/databasemeta"; then
+  :
+else
+ exit 1
+fi
+
+moosh debug-off
+if ! curl http://127.0.0.1/$moodle_url/ | grep "core/databasemeta"; then
+  exit 0 
+else
+  exit 1
+fi
+
