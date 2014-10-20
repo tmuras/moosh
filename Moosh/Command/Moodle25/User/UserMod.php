1c899ff288b5cc5f6613e5a894f1d4478cb5085c
Kacper Golewski <k.golewski@gmail.com>
2014-10-20 02:13:16 +0200

    add command user-mod
    argument: username
    
    options:
    	-g 	set given user as super user.

---
+++ Moosh/Command/Moodle25/User/UserMod.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * moosh - Moodle Shell
+ *
+ * @copyright  2012 onwards Tomasz Muras
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author     Kacper Golewski <k.golewski@gmail.com>
+ */
+
+
+namespace Moosh\Command\Moodle25\User;
+use Moosh\MooshCommand;
+
+class UserMod extends MooshCommand
+{
+    public function __construct()
+    {
+        parent::__construct('mod', 'user');
+
+        $this->addOption('g|global', 'user will be set as super user.');
+
+        $this->addArgument('username');
+    }
+
+    public function execute()
+    {   
+        global $CFG;
+        $options = $this->expandedOptions;
+        $user = get_user_by_name($this->arguments[0]);
+
+        if ($user) {
+            $user_id = $user->id;
+        } else {
+            cli_error("No user found.");
+        }
+
+        if ($options['global']) {
+            foreach(explode(',', $CFG->siteadmins) as $admin) {
+                $admin = (int)$admin;
+                if ($admin) {
+                    $admins[$admin] = $admin;
+                }
+            }
+
+            if (isset($admins[$user_id])) {
+                unset($admins[$user_id]);
+            }
+            array_unshift($admins, $user_id);
+            set_config('siteadmins', implode(',', $admins));
+            purge_all_caches();
+
+            echo "User $user->username is set as global super user.\n";
+        }
+
+
+        
+    }
+}
