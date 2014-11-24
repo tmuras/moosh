d357e1bcbfb0f4127b15f5d30c79d63c044e7ef2
Tomasz Muras <nexor1984@gmail.com>
2014-11-24 12:57:41 +0100

    Deprecate command CourseBulkUnenrolInactiveUsers.php.

--- Moosh/Command/Moodle23/Course/CourseBulkUnenrolInactiveUsers.php
+++ Moosh/Command/Moodle23/Course/CourseBulkUnenrolInactiveUsers.php
@@ -24,47 +24,6 @@ class CourseBulkUnenrolInactiveUsers extends MooshCommand {
     }
 
     public function execute() {
-
-
-        global $CFG, $DB, $PAGE;
-
-        require_once($CFG->dirroot . '/enrol/locallib.php');
-        require_once($CFG->dirroot . '/group/lib.php');
-
-        $options = $this->expandedOptions;
-        $arguments = $this->arguments;
-
-        $course = $DB->get_record('course', array('id' => $arguments[0]), '*', MUST_EXIST);
-        $context = context_course::instance($course->id);
-        $manager = new course_enrolment_manager($PAGE, $course);
-
-        #remove all enrolled users who never accessed the course
-        try {
-            foreach (explode(',', $options['role']) as $role) {
-                if (!in_array($role, array('student','teacher', 'manager', 'editingteacher'))) {
-                    exit('role is not valid');
-                }
-                $role = $DB->get_record('role', array('shortname' => $role));
-                $context = context_course::instance($course->id);
-                $users = get_role_users($role->id, $context);
-                #unenrol inactive users
-                foreach ($users as $user) {
-                    $lastaccessexists = false;
-                    $lastaccessexists = $DB->record_exists('user_lastaccess', 
-                                       array('courseid'=>$course->id, 'userid'=>$user->id));
-                    if(!$lastaccessexists) {
-		        $enrolments = $manager->get_user_enrolments($user->id);
-		        foreach ($enrolments as $enrolment) {
-		            list ($instance, $plugin) = $manager->get_user_enrolment_components($enrolment);
-		            if ($instance && $plugin && $plugin->allow_unenrol_user($instance, $enrolment)) {
-		                $plugin->unenrol_user($instance, $enrolment->userid);
-		            }
-		        }
-                    }
-                }
-            }
-        } catch (Exception $e) {
-            print get_class($e) . " thrown within the exception handler. Message: " . $e->getMessage() . " on line " . $e->getLine();
-        }
+        cli_error('This command has been deprecated. Use user-list & course-unenrol instead.');
     }
 }
