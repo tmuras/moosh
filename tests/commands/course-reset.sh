c093d4217b9b3fe18963379db8bc8e6c5123a2ec
Robert Szymoniak <djxr24@gmail.com>
2015-08-07 09:15:20 +0200

    Created test named course-reset

--- tests/commands/course-reset.sh
+++ tests/commands/course-reset.sh
@@ -1,16 +1,35 @@
-#!/bin/bash
+#!/bin/bash -x
 source functions.sh
 
 install_db
 install_data
 cd $MOODLEDIR
 
-$MOOSHCMD course-reset 2
+$MOOSHCMD course-reset -s "reset_gradebook_grades=0" 3
+if  !(mysql -u "$DBUSER" -p"$DBPASSWORD" "$DBNAME" -e \
+    "SELECT * FROM mdl_grade_grades WHERE rawgrade='50.00000'"\
+    | grep 50.00000); then
+exit 1
 
-# Check if user got un-enrolled from the course reset
-if echo "SELECT userid FROM mdl_user_enrolments WHERE enrolid=1 and userid=4\G" \
-    | mysql -u "$DBUSER" -p"$DBPASSWORD" "$DBNAME" | grep "userid: 4"; then
+fi
+
+$MOOSHCMD course-reset -s "reset_gradebook_grades=1" 3
+if  mysql -u "$DBUSER" -p"$DBPASSWORD" "$DBNAME" -e \
+    "SELECT * FROM mdl_grade_grades WHERE rawgrade='50.00000'"\
+    | grep 50.00000; then
+exit 1
+
+fi
+$MOOSHCMD course-reset -s "reset_assign_submissions=1" 3
+ if  mysql -u "$DBUSER" -p"$DBPASSWORD" "$DBNAME" -e \
+    "SELECT * FROM mdl_assign_submission WHERE status='submitted'"\
+    | grep submitted; then
   exit 1
-else
-  exit 0
+fi
+$MOOSHCMD course-reset -s "unenrol_users=5," 3
+if  mysql -u "$DBUSER" -p"$DBPASSWORD" "$DBNAME" -e \
+    "SELECT * FROM mdl_user_enrolments WHERE id='3'"\
+    | grep 3; then
+  exit 1
+
 fi
