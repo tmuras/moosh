9d57fed21790ca65dafa07a67e295601653c81bb
Tomasz Muras <nexor1984@gmail.com>
2018-04-13 20:12:14 +0200

    Add lang and install.xml

--- Moosh/Command/Moodle33/Dev/GenerateModule.php
+++ Moosh/Command/Moodle33/Dev/GenerateModule.php
@@ -23,6 +23,7 @@ class GenerateModule extends MooshCommand
     {
         require_once($this->mooshDir.'/vendor/mudrd8mz/moodle-tool_pluginskel/vendor/autoload.php');
         require_once($this->mooshDir.'/vendor/mudrd8mz/moodle-tool_pluginskel/locallib.php');
+        require_once($this->mooshDir.'/vendor/mudrd8mz/moodle-tool_pluginskel/classes/local/util/exception.php');
         require_once($this->mooshDir.'/vendor/mudrd8mz/moodle-tool_pluginskel/classes/local/util/manager.php');
         require_once($this->mooshDir.'/vendor/mudrd8mz/moodle-tool_pluginskel/classes/local/util/mustache.php');
         require_once($this->mooshDir.'/vendor/mudrd8mz/moodle-tool_pluginskel/classes/local/skel/base.php');
@@ -40,8 +41,8 @@ class GenerateModule extends MooshCommand
         }
 
         //copy newmodule
-        $modpath = $this->topDir.'/mod/'.$this->arguments[0];
-        $modname = "mod_".$this->arguments[0];
+        $modname = $this->arguments[0];
+        $modpath = $this->topDir.'/mod/'.$modname;
         if (file_exists($modpath)) {
             cli_problem("Already exists: '$modpath'");
             cli_problem("Not creating new module ".$this->arguments[0]);
@@ -52,12 +53,12 @@ class GenerateModule extends MooshCommand
 
         $recipe = array(
                 'copyright' => '2018 Your Name <you@example.com>',
-                'component' => 'mod_xxx',
+                'component' => "$modname",
                 'release' => '0.1.0',
-                'version' => 2018040200,
+                'version' => date('Ymd00'),
                 'requires' => '2017111300',
                 'maturity' => 'MATURITY_ALPHA',
-                'name' => 'Module XXX',
+                'name' => "Module $modname",
                 'mod_features' =>
                         array(
                                 'gradebook' => true,
@@ -81,13 +82,90 @@ class GenerateModule extends MooshCommand
                                                 'filename' => 'sample_cli',
                                         ),
                         ),
+                'lang_strings' =>
+                        array (
+                                array (
+                                        'id' => "{$modname}name",
+                                        'text' => $modname
+                                ),
+                                array (
+                                        'id' => "{$modname}name_help",
+                                        'text' => $modname
+                                ),
+                                array (
+                                        'id' => "{$modname}settings",
+                                        'text' => "{$modname}settings"
+                                ),
+                                array (
+                                        'id' => "{$modname}fieldset",
+                                        'text' => "{$modname}fieldset"
+                                ),
+                                array (
+                                        'id' => "missingidandcmid",
+                                        'text' => "Missing id and cmid"
+                                ),
+                                array (
+                                        'id' => "modulename",
+                                        'text' => $modname
+                                ),
+                                array (
+                                        'id' => "modulename_help",
+                                        'text' => $modname
+                                ),
+                                array (
+                                        'id' => "modulenameplural",
+                                        'text' => $modname
+                                ),
+                                array (
+                                        'id' => "nonewmodules",
+                                        'text' => "No new modules"
+                                ),
+                                array (
+                                        'id' => "pluginadministration",
+                                        'text' => "Plugin administration"
+                                ),
+                                array (
+                                        'id' => "view",
+                                        'text' => "View"
+                                ),
+                        ),
         );
 
         $logger = new \Monolog\Logger('tool_pluginskel');
         $logger->pushHandler(new \Monolog\Handler\StreamHandler('php://stdout', constant('\Monolog\Logger::NOTICE')));
-        $manager = \tool_pluginskel\local\util\manager::instance($logger);
+        $manager = \tool_pluginskel\local\util\manager::instance($logger,$this->mooshDir . '/vendor/mudrd8mz/moodle-tool_pluginskel/skel');
         $manager->load_recipe($recipe);
         $manager->make();
-        $manager->write_files('/tmp/a');
+        $manager->write_files($modpath);
+
+        $installfile = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
+<XMLDB PATH=\"mod/$modname/db\" VERSION=\"20101203\" COMMENT=\"XMLDB file for Moodle mod/$modname\"
+    xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"
+    xsi:noNamespaceSchemaLocation=\"../../../lib/xmldb/xmldb.xsd\"
+>
+  <TABLES>
+    <TABLE NAME=\"$modname\" COMMENT=\"Default comment for $modname, please edit me\">
+      <FIELDS>
+        <FIELD NAME=\"id\" TYPE=\"int\" LENGTH=\"10\" NOTNULL=\"true\" UNSIGNED=\"true\" SEQUENCE=\"true\"/>
+        <FIELD NAME=\"course\" TYPE=\"int\" LENGTH=\"10\" NOTNULL=\"true\" UNSIGNED=\"true\" SEQUENCE=\"false\" COMMENT=\"Course newmodule activity belongs to\"/>
+        <FIELD NAME=\"name\" TYPE=\"char\" LENGTH=\"255\" NOTNULL=\"true\" SEQUENCE=\"false\" COMMENT=\"name field for moodle instances\"/>
+        <FIELD NAME=\"intro\" TYPE=\"text\" NOTNULL=\"true\" SEQUENCE=\"false\" COMMENT=\"General introduction of the newmodule activity\"/>
+        <FIELD NAME=\"introformat\" TYPE=\"int\" LENGTH=\"4\" NOTNULL=\"true\" UNSIGNED=\"true\" DEFAULT=\"0\" SEQUENCE=\"false\" COMMENT=\"Format of the intro field (MOODLE, HTML, MARKDOWN...)\"/>
+        <FIELD NAME=\"timecreated\" TYPE=\"int\" LENGTH=\"10\" NOTNULL=\"true\" UNSIGNED=\"true\" SEQUENCE=\"false\"/>
+        <FIELD NAME=\"timemodified\" TYPE=\"int\" LENGTH=\"10\" NOTNULL=\"true\" UNSIGNED=\"true\" DEFAULT=\"0\" SEQUENCE=\"false\"/>
+        <FIELD NAME=\"grade\" TYPE=\"int\" LENGTH=\"10\" NOTNULL=\"true\" DEFAULT=\"100\" SEQUENCE=\"false\" COMMENT=\"The maximum grade. Can be negative to indicate the use of a scale.\"/>
+      </FIELDS>
+      <KEYS>
+        <KEY NAME=\"primary\" TYPE=\"primary\" FIELDS=\"id\"/>
+      </KEYS>
+      <INDEXES>
+        <INDEX NAME=\"course\" UNIQUE=\"false\" FIELDS=\"course\"/>
+      </INDEXES>
+    </TABLE>
+  </TABLES>
+</XMLDB>
+";
+
+        file_put_contents($modpath . '/db/install.xml', $installfile);
     }
 }
