e2548f210b9ceb8fc43be14ef44b04e70e71fe63
Tomasz Muras <nexor1984@gmail.com>
2018-04-25 13:50:59 +0200

    Version bump to 0.26

--- moosh.php
+++ moosh.php
@@ -37,7 +37,7 @@ use GetOptionKit\OptionCollection;
 @error_reporting(E_ALL | E_STRICT);
 @ini_set('display_errors', '1');
 
-define('MOOSH_VERSION', '0.25');
+define('MOOSH_VERSION', '0.26');
 define('MOODLE_INTERNAL', true);
 
 $appspecs = new OptionCollection;
