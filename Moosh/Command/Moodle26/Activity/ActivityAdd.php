b88d2f59be8c01f16d3fe879fc2f6845be98f3b1
Dr Bean <drbean@sac.nuu.edu.tw>
2018-05-20 12:11:24 +0800

    moduledata.gradecat=options.gradecat

--- Moosh/Command/Moodle26/Activity/ActivityAdd.php
+++ Moosh/Command/Moodle26/Activity/ActivityAdd.php
@@ -66,6 +66,9 @@ class ActivityAdd extends MooshCommand
         if (!empty($options['idnumber'])) {
             $moduledata->idnumber = $options['idnumber'];
         }
+        if (!empty($options['gradecat'])) {
+            $moduledata->gradecat = $options['gradecat'];
+        }
 
         $moduledata->section = $options['section'];
 
