08e00c5635b26988da486c65c202db1e8da16657
Tomasz Muras <tomek.muras@enovation.ie>
2019-03-17 20:43:52 +0100

    Timezone support.

--- Moosh/Command/Moodle31/Report/ReportConcurrency.php
+++ Moosh/Command/Moodle31/Report/ReportConcurrency.php
@@ -26,8 +26,7 @@ class ReportConcurrency extends MooshCommand {
         parent::__construct('concurrency', 'report');
 
         $this->addOption('f|from:',
-                'from date in YYYYMMDD or YYYY-MM-DD format (default is 30 days backwards)',
-                '-30 days'
+                'from date in YYYYMMDD or YYYY-MM-DD format (default is 1 month ago)'
         );
         $this->addOption('t|to:',
                 'to date in YYYYMMDD or YYYY-MM-DD format (default is today)');
@@ -35,8 +34,13 @@ class ReportConcurrency extends MooshCommand {
         $this->addOption('z|time-zone:',
                 'timezone used to display the dates. Possible values on https://secure.php.net/manual/en/timezones.php.', "UTC");
         $this->addOption('c|csv:', 'save the concurrency values to CSV file.');
-        $this->addOption('z|zero-days-include:',
+        $this->addOption('Z|zero-days-include:',
                 'By default the days with no activity at all are ignored in some statistics. This option reverts that.');
+        $this->addOption('work-hours-from:',
+                '(not implemented yet) The start time for generating the statistics. Values from 0 to 23.');
+        $this->addOption('work-hours-to:',
+                '(not implemented yet) The start time for generating the statistics. Values from 0 to 23.');
+        $this->addOption('work-days:', '(not implemented yet) Define working days.', '12345');
 
     }
 
@@ -45,25 +49,47 @@ class ReportConcurrency extends MooshCommand {
 
         $options = $this->expandedOptions;
 
-        $from_date = strtotime($options['from']);
-        if ($options['to']) {
-            $to_date = strtotime($options['to']);
+        $timezone = new \DateTimeZone($options['time-zone']);
+        $fromdate = str_replace('-', '', $options['from']);
+        if (strlen($fromdate) != 8) {
+            cli_error("Wrong format for --from. Use YYYYMMDD or YYYY-MM-DD");
+        }
+        if (!$fromdate) {
+            // Set to 1 month ago.
+            $fromdate = new \DateTime('now', $timezone);
+            $fromdate->setTime(0, 0, 0);
+            $fromdate->sub(new \DateInterval('P1M'));
         } else {
-            $to_date = time();
+            $fromdate = new \DateTime($fromdate, $timezone);
         }
 
-        if ($from_date === false) {
-            cli_error('invalid from date');
+        $todate = str_replace('-', '', $options['to']);
+        if (strlen($todate) != 8) {
+            cli_error("Wrong format for --to. Use YYYYMMDD or YYYY-MM-DD");
         }
 
-        if ($to_date === false) {
-            cli_error('invalid to date');
+        if (!$todate) {
+            // Set to 1 month ago.
+            $todate = new \DateTime('now', $timezone);
+        } else {
+            $todate = new \DateTime($todate, $timezone);
+        }
+        $todate->setTime(23, 59, 59);
+
+        if ($todate < $fromdate) {
+            cli_error('"to date" must be later than "from date".');
         }
 
-        if ($to_date < $from_date) {
-            cli_error('to date must be higher than from date');
+        if ($this->verbose) {
+            echo 'Records from ' .
+                    $fromdate->format(self::DATE_FORMAT) . ' [' . $fromdate->getTimestamp() . ']' .
+                  ' to ' .
+                    $todate->format(self::DATE_FORMAT) . ' [' . $todate->getTimestamp() . ']' . "\n";
         }
 
+        $tsutcfrom = $fromdate->getTimestamp();
+        $tsutcto = $todate->getTimestamp();
+        
         if ($options['csv']) {
             $filepath = $this->cwd . '/' . $options['csv'];
             $csvfile = fopen($filepath, 'w');
@@ -72,8 +98,19 @@ class ReportConcurrency extends MooshCommand {
             }
         }
 
-        $timezone = new \DateTimeZone($options['time-zone']);
         $period = $options['period'] * 60;
+
+        // Number of entries in the log for given from-to range.
+        $sql = "SELECT COUNT(*) AS 'count', MIN(id) AS 'min', MAX(id) AS 'max'
+                   FROM {logstore_standard_log}
+				  WHERE timecreated BETWEEN $tsutcfrom AND $tsutcto";
+        $record = $DB->get_record_sql($sql);
+        $recordsfrom = $record->min;
+        $recordsto = $record->max;
+        $recordscount = $record->count;
+        if ($this->verbose) {
+            echo "To retrieve $recordscount records from id $recordsfrom to $recordsto\n";
+        }
 
         // Get the number of concurrent users for each period.
         $sql = "SELECT 
@@ -83,7 +120,7 @@ class ReportConcurrency extends MooshCommand {
 				(SELECT ROUND( timecreated / ( $period ) ) AS period,
 				COUNT( DISTINCT userid ) AS online_users
 				FROM {logstore_standard_log}
-				WHERE timecreated BETWEEN $from_date AND $to_date
+				WHERE timecreated BETWEEN $tsutcfrom AND $tsutcto
 				AND origin = 'web'
 				GROUP BY period
 				) AS concurrent_users_report";
@@ -111,18 +148,12 @@ class ReportConcurrency extends MooshCommand {
             $previoustime = $v->unixtime;
         }
 
-        $weekstats = new weekday_stats();
+        $weekstats = new weekday_stats($options['zero-days-include']);
 
-        $day = null;
         $maxconcurrent = ['date' => null, 'users' => 0];
         foreach ($fulldata as $k => $row) {
             $weekstats->add($row['date'], $row['users']);
-            echo $row['date']->format(self::DATE_FORMAT) . "\n";
-            $currentweekday = $row['date']->format("N");
-            if (!$day || $currentweekday != $day) {
-                echo "Switching to the next working day: $currentweekday\n";
-                $day = $currentweekday;
-            }
+            echo $row['date']->format(self::DATE_FORMAT), " - ", $row['users'], "\n";
 
             if ($options['csv']) {
                 fputcsv($csvfile, [$row['date']->format(self::DATE_FORMAT), $row['users']]);
@@ -137,18 +168,10 @@ class ReportConcurrency extends MooshCommand {
             fclose($csvfile);
         }
 
-        // display the instance name
         echo "Name: " . $CFG->wwwroot . "\n";
 
-        // display the size of the data folder
-        $dataroot = run_external_command("du -bs $CFG->dataroot", "Couldn't find dataroot directory");
-        $pattern = '/\d*/';
-        preg_match($pattern, $dataroot[0], $matches);
-
-        echo "Data Size: " . round($matches[0] / 1024 / 1024, 2) . " (MB)\n";
-
-        // display database size
-        $sql = "SELECT table_name AS 'Table',
+        // Database size
+        $sql = "SELECT table_name AS 'table',
                   ROUND(((data_length + index_length))) AS 'Size(Bytes)'
                   FROM information_schema.TABLES
                   WHERE table_schema = '" . $CFG->dbname . "'
@@ -156,61 +179,51 @@ class ReportConcurrency extends MooshCommand {
         $results = $DB->get_records_sql($sql);
 
         $databasesize = 0;
+        $topsizes = [];
+        $topnames = [];
         foreach ($results as $result) {
+            if (!isset($topsizes[0])) {
+                $topsizes[0] = $result->{'size(bytes)'};
+                $topnames[0] = $result->table;
+            } else if (!isset($topsizes[1])) {
+                $topsizes[1] = $result->{'size(bytes)'};
+                $topnames[1] = $result->table;
+            } else if (!isset($topsizes[2])) {
+                $topsizes[2] = $result->{'size(bytes)'};
+                $topnames[2] = $result->table;
+            }
             $databasesize += $result->{'size(bytes)'};
         }
-        echo "Database Size: " . round($databasesize / 1024 / 1024, 2) . " (MB)\n";
+        echo "Database Size: $databasesize\n";
+        echo "Top 3 tables:\n";
+        for ($i = 0; $i < 3; $i++) {
+            echo "\t" . $topnames[$i] . " : " . $topsizes[$i] . "\n";
+        }
 
-        // display active users during specified period
+        // Display active users during specified period.
         $sql = "SELECT COUNT( DISTINCT userid ) AS NumberOfActiveUsers
 				FROM {logstore_standard_log}
-				WHERE timecreated BETWEEN $from_date AND $to_date";
+				WHERE timecreated BETWEEN $tsutcfrom AND $tsutcto";
         $result = $DB->get_record_sql($sql);
 
         echo "Active Users: " . $result->{'numberofactiveusers'} . "\n";
+        echo "Max Concurrent Users: " . $maxconcurrent['users'] . "\n";
+        echo "\ton " . $maxconcurrent['date']->format(self::DATE_FORMAT) . "\n";
+        $stats = $weekstats->get_stats();
+        print_r($stats);
 
-        // get the max concurrent users during any period
-        $sql = "SELECT MAX( concurrent_users_report.online_users ) AS maxusercount
-                FROM (SELECT ROUND( timecreated / ( $period ) ) AS period,
-				COUNT( DISTINCT userid ) AS online_users
-				FROM {logstore_standard_log}
-				WHERE timecreated BETWEEN $from_date AND $to_date
-				GROUP BY period) AS concurrent_users_report";
-        $result = $DB->get_record_sql($sql);
-
-        echo "Max Concurrent Users: " . $result->maxusercount . "\n";
-
-        // Get the average concurrent users last 12 months
-        $todayminustwelvemonths = strtotime('-1 years');
-        $totalusersinpastyear = 0;
-        $periodsoveryear = 0;
-
-        $sql = "SELECT (FROM_UNIXTIME(period * ( $period ))) AS Date,
-                DAYNAME( FROM_UNIXTIME( period * ( $period ) ) ) AS DAY,
-                DATE_FORMAT( FROM_UNIXTIME( period * ( $period ) ) , '%M %d, %Y' ) AS DayDate,
-                TIME( FROM_UNIXTIME( period * ( $period ) ) ) AS Timecreated,
-				online_users FROM 
-				(SELECT ROUND( timecreated / ( $period ) ) AS period,
-				COUNT( DISTINCT userid ) AS online_users
-				FROM {logstore_standard_log}
-				WHERE timecreated > $todayminustwelvemonths
-				GROUP BY period) AS concurrent_users_report";
-        $results = $DB->get_records_sql($sql);
-
-        foreach ($results as $result) {
-            $totalusersinpastyear += $result->online_users;
-            $periodsoveryear++;
-        }
-        if ($periodsoveryear == 0) {
-            $periodsoveryear = 1;
-        }
-
-        echo "Average concurrent users past 12 months: " . round($totalusersinpastyear / $periodsoveryear, 2) . "\n";
+        //echo "Average concurrent users: " . round($totalusersinpastyear / $periodsoveryear, 2) . "\n";
     }
 }
 
 class weekday_stats {
     private $daily = [];
+    private $weekdays = [];
+    private $zerodaysinclude = false;
+
+    public function __construct($zerodaysinclude) {
+        $this->zerodaysinclude = $zerodaysinclude;
+    }
 
     /**
      * Add the value of $users
@@ -220,10 +233,10 @@ class weekday_stats {
      */
     public function add(\DateTime $date, $users) {
         // Let's store each day separately.
-        // z The day of the year 0 - 365.
+        // z - The day of the year 0 - 365.
         $dayyear = $date->format('z');
         if (!isset($this->daily[$dayyear])) {
-            $this->daily[$dayyear] = ['count' => 0, 'sum' => 0, 'max' => 0];
+            $this->daily[$dayyear] = ['count' => 0, 'sum' => 0, 'max' => 0, 'date' => $date];
         }
         $this->daily[$dayyear]['count']++;
         $this->daily[$dayyear]['sum'] += $users;
@@ -235,9 +248,27 @@ class weekday_stats {
     public function get_stats() {
         // Now that we have all data, calculate averages.
         foreach ($this->daily as $day => $data) {
-            $this->daily[$day]['avg'] = round($data['sum'] / $data['avg'],2);
+            $this->daily[$day]['avg'] = round($data['sum'] / $data['count'], 2);
         }
 
         // Generate statistics per week day.
+        // N - Week day.
+        foreach ($this->daily as $day => $data) {
+            if (!$this->zerodaysinclude && $data['sum'] == 0) {
+                continue;
+            }
+            $currentweekday = $data['date']->format("N");
+            if (!isset($this->weekdays[$currentweekday])) {
+                $this->weekdays[$currentweekday] = ['count' => 0, 'sum' => 0];
+            }
+            $this->weekdays[$currentweekday]['count']++;
+            $this->weekdays[$currentweekday]['sum'] += $data['avg'];
+        }
+
+        foreach ($this->weekdays as $day => $data) {
+            $this->weekdays[$day]['avg'] = round($data['sum'] / $data['count'], 2);
+        }
+
+        return $this->weekdays;
     }
 }
