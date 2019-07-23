a6a9442227de49ed944c4cf21ebc3f4f325711dc
kallabruni <k.chelstowska@gmail.com>
2019-07-23 16:29:07 +0200

    update user-list.feature and course-list.feature

--- tests/mooshtest/tests/behat/behat_moosh.php
+++ tests/mooshtest/tests/behat/behat_moosh.php
@@ -25,7 +25,7 @@
 
 // NOTE: no MOODLE_INTERNAL test here, this file may be required by behat before including /config.php.
 
-require_once(__DIR__ . '/../../../../lib/behat/behat_base.php');
+require_once(__DIR__ . '/../../../../../lib/behat/behat_base.php');
 
 use Behat\Mink\Exception\ExpectationException;
 
