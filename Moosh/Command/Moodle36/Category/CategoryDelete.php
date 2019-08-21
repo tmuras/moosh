b49f6b28fb7826d4c2ed39bbb1d302d72a4fa7d6
Marty Gilbert <martygilbert@gmail.com>
2019-08-21 07:51:43 -0400

    Test for valid catid values

--- Moosh/Command/Moodle36/Category/CategoryDelete.php
+++ Moosh/Command/Moodle36/Category/CategoryDelete.php
@@ -23,10 +23,15 @@ class CategoryDelete extends MooshCommand
 
 		$catid = $this->arguments[0];
 
+		//Make sure catid is valid.
+		if ($catid <= 0) {
+			echo "Invalid category id. Must be > 0\n";
+		}
+
 		//Make sure the category exists
 		if ($DB->record_exists('course_categories', array('id' => $catid))) {
 
-			$category = core_course_category::get($categoryid);
+			$category = \core_course_category::get($catid);
 
 			//do a recursive delete of all courses and subcats
 			$courses = $category->delete_full(false);
