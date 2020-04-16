9e3210c21e37bb7fc09d136873cb319b6c849917
Olena Tatarintseva <olena.tatarintseva@enovation.ie>
2020-04-16 12:30:34 +0200

    admin-remove-orphaned-modules command refactored

--- Moosh/Command/Moodle35/Admin/RemoveOrphanedModules.php
+++ Moosh/Command/Moodle35/Admin/RemoveOrphanedModules.php
@@ -1,10 +1,9 @@
 <?php
 /**
- * moosh admin-remove-orphaned-modules [--removeall
- *                   modulesmap1=<modulesmaptable1> [modulesmap2=<modulesmaptable2> ...]
+ * moosh admin-remove-orphaned-modules [--removeall]
  *
- * @example sudo -u www-data  moosh admin-remove-orphaned-modules  -r all module_name_1=module_table_name_1 module_name_2=module_table_name_2
- * @example sudo -u www-data  moosh admin-remove-orphaned-modules  -remove=90,105,1999 module_name_1=module_table_name_1 module_name_2=module_table_name_2
+ * @example sudo -u www-data  moosh admin-remove-orphaned-modules  -r all
+ * @example sudo -u www-data  moosh admin-remove-orphaned-modules  -remove=90,105,1999
  *
  * @copyright  2012 onwards Tomasz Muras
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
@@ -23,9 +22,8 @@ class RemoveOrphanedModules extends MooshCommand
 
         $this->addOption('r|remove', 'remove found orphaned modules: all or given ids separated by comma', 0);
 
-        $this->addArgument('modulesmap');
         $this->minArguments = 0;
-        $this->maxArguments = 100;
+        $this->maxArguments = 1;
     }
 
     public function execute() {
@@ -38,24 +36,15 @@ class RemoveOrphanedModules extends MooshCommand
         if ($options['remove']) {
             $remove = (bool)$options['remove'];
         }
-
         //input data parsing
-        $mapping = [];
-        foreach ($this->arguments as $argument) {
-            $expanded = explode('=', $argument);
-            if (count($expanded) == 2) {
-                $mapping[$expanded[0]] = $expanded[1];
-            } else if ($remove === true) {
-                if ($argument === 'all') {
-                    $removeall = true;
-                } else {
-                    if (strpos(',', $argument) === false && (int)$argument != $argument) {
-                        print_error("Wrong 'remove' option format. Please, pass option value like: course_module_id_1,course_module_id_2,course_module_id_3 or 'all'");
-                    }
-                    $remove = explode(',', $argument);
-                }
+        if ($remove === true) {
+            if ($this->arguments[0] === 'all') {
+                $removeall = true;
             } else {
-                print_error("Wrong arguments format. Please, pass arguments like: module_name=module_instances_table_name_without_prefix");
+                if (strpos(',', $this->arguments[0]) === false && (int)$this->arguments[0] != $this->arguments[0]) {
+                    print_error("Wrong 'remove' option format. Please, pass option value like: course_module_id_1,course_module_id_2,course_module_id_3 or 'all'");
+                }
+                $remove = explode(',', $this->arguments[0]);
             }
         }
 
@@ -68,52 +57,52 @@ class RemoveOrphanedModules extends MooshCommand
             (
                 SELECT id
                 FROM mdl_modules
-                WHERE name = \'{module_name}\'
+                WHERE name = :module_name
             )';
-        $adhocquery = 'SELECT *, FROM_UNIXTIME(nextruntime) as nextruntimestr FROM {task_adhoc} WHERE classname = \'\\\core_course\\\task\\\course_delete_modules\' AND customdata LIKE \'%"course":"{courseid}","module":"{moduleid}","instance":"{instanceid}"%\'';
+        $adhocquery = 'SELECT *, FROM_UNIXTIME(nextruntime) as nextruntimestr FROM {task_adhoc} WHERE classname = \'\\\core_course\\\task\\\course_delete_modules\' AND customdata LIKE \'%"module":"{moduleid}"%\'';
 
         $tables = $DB->get_tables();
 
         $modules = $DB->get_records('modules');
         //check all orphaned modules records
         foreach ($modules as $module) {
-            if (!in_array($module->name, $tables) && !isset($mapping[$module->name])) {
+            if (!in_array($module->name, $tables)) {
                 echo 'Table does not exists: ' . $module->name . "\n";
-            } else {
-                $moduleselectsql = str_replace('{module_name}', $module->name, $selectquery);
-                if (isset($mapping[$module->name])) {
-                    $moduleselectsql = str_replace('{module_table_name}', $CFG->prefix . $mapping[$module->name], $moduleselectsql);
-                } else {
-                    $moduleselectsql = str_replace('{module_table_name}', $CFG->prefix . $module->name, $moduleselectsql);
+                continue;
+            }
+            $moduleselectsql = str_replace('{module_table_name}', $CFG->prefix . $module->name, $selectquery);
+
+            $orphanedrows = $DB->get_records_sql($moduleselectsql, array('module_name' => $module->name));
+            if ($orphanedrows) {
+                $moduleadhocquery = str_replace('{moduleid}', (int)$module->id, $adhocquery);
+                $adhocs = $DB->get_records_sql($moduleadhocquery); //, array('moduleid' => (int)$module->id));
+                $adhocstmp = array();
+                foreach ($adhocs as $adhoc) {
+                    preg_match('/\"course\":\"(\d*)\",.*\"instance\":\"(\d*)\"/', $adhoc->customdata, $matches);
+                    $adhocstmp[$matches[1] . '_' . $matches[2]] = $adhoc;
                 }
-
-                $orphanedrows = $DB->get_records_sql($moduleselectsql);
-                if ($orphanedrows) {
-                    echo 'Module: ' . $module->name . "\n";
-                    foreach ($orphanedrows as $orphanedrow) {
-                        $instanceadhocquery = str_replace('{courseid}', $orphanedrow->course, $adhocquery);
-                        $instanceadhocquery = str_replace('{moduleid}', $orphanedrow->module, $instanceadhocquery);
-                        $instanceadhocquery = str_replace('{instanceid}', $orphanedrow->instance, $instanceadhocquery);
-                        $adhoc = $DB->get_record_sql($instanceadhocquery);
-
-                        $adhocdetails = ' --- NO adhoc cron task found';
-                        if ($adhoc) {
-                            $adhocdetails = ' --- Next cron run: ' . $adhoc->nextruntimestr;
-                            $lastperformstatus = ', was NOT performed yet';
-                            if ($adhoc->faildelay > 0) {
-                                $lastperformstatus = ', last perform status: FAILED';
-                            }
-                            $adhocdetails .= $lastperformstatus;
+                $adhocs = $adhocstmp;
+                $adhocstmp = null;
+                echo 'Module: ' . $module->name . "\n";
+                foreach ($orphanedrows as $orphanedrow) {
+                    $adhocdetails = ' --- NO related adhoc cron task found';
+                    $adhockey = $orphanedrow->course . '_' . $orphanedrow->instance;
+                    if (isset($adhocs[$adhockey])) {
+                        $adhocdetails = ' --- Next cron run: ' . $adhocs[$adhockey]->nextruntimestr;
+                        $lastperformstatus = ', was NOT performed yet';
+                        if ($adhocs[$adhockey]->faildelay > 0) {
+                            $lastperformstatus = ', last perform status: FAILED';
                         }
-                        echo ' - id=' . $orphanedrow->id . ', course=' . $orphanedrow->course . ', instance=' . $orphanedrow->instance . $adhocdetails . "\n";
+                        $adhocdetails .= $lastperformstatus;
+                    }
+                    echo ' - id=' . $orphanedrow->id . ', course=' . $orphanedrow->course . ', instance=' . $orphanedrow->instance . $adhocdetails . "\n";
 
-                        // remove records if required
-                        if ((!$removeall && $remove && in_array($orphanedrow->id, $remove)) || $removeall) {
-                            if ($DB->delete_records('course_modules', array('id' => $orphanedrow->id))) {
-                                echo '   - Removed.' . "\n";
-                            } else {
-                                echo ' - course module removing ERROR' . "\n";
-                            }
+                    // remove records if required
+                    if ((!$removeall && $remove && in_array($orphanedrow->id, $remove)) || $removeall) {
+                        if ($DB->delete_records('course_modules', array('id' => $orphanedrow->id))) {
+                            echo '   - Removed.' . "\n";
+                        } else {
+                            echo ' - course module removing ERROR' . "\n";
                         }
                     }
                 }
