f12a1e3220e6652425e3376b6456dae1177a2b35
Tomasz Muras <tomek.muras@enovation.ie>
2021-05-20 17:26:44 +0200

    New command: task-lock-check

--- Moosh/Command/Moodle39/Task/LockCheck.php
+++ Moosh/Command/Moodle39/Task/LockCheck.php
@@ -7,45 +7,54 @@
  */
 
 namespace Moosh\Command\Moodle39\Task;
+
 use Moosh\MooshCommand;
 
-class LockCheck extends MooshCommand
-{
-    public function __construct()
-{
-    parent::__construct('lock-check', 'task');
+class LockCheck extends MooshCommand {
+    public function __construct() {
+        parent::__construct('lock-check', 'task');
 
-    //$this->addArgument('name');
+        //$this->addArgument('name');
 
-    //$this->addOption('t|test', 'option with no value');
-    //$this->addOption('o|option:', 'option with value and default', 'default');
+        $this->addOption('a|all', 'show all locks checked');
+        //$this->addOption('o|option:', 'option with value and default', 'default');
 
-}
-
-    public function execute()
-{
-    // Some variables you may want to use
-    //  $this->cwd - the directory where moosh command was executed
-    //  $this->mooshDir - moosh installation directory
-    //  $this->expandedOptions - commandline provided options, merged with defaults
-    //  $this->topDir - top Moodle directory
-    //  $this->arguments[0] - first argument passed
-    //  $this->pluginInfo - array with information about the current plugin (based on cwd), keys:'type','name','dir'
-    //  $this->verbose - if set to true, then "moosh -v" was run - add more verbose / debug information
-
-    $options = $this->expandedOptions;
-
-    // Get the list of all tasks
-
-    // calculate lock name
-    // sha1($this->dbprefix . $resource);
-    // Example
-    // \mod_assign\task\cron_task
-    // 68ad53033898d7104066b1bdb33524986c97c870
-    /* if verbose mode was requested, show some more information/debug messages
-    if($this->verbose) {
-        echo "Say what you're doing now";
     }
-    */
-}
+
+    public function execute() {
+        global $CFG, $DB;
+
+        // Some variables you may want to use
+        //  $this->cwd - the directory where moosh command was executed
+        //  $this->mooshDir - moosh installation directory
+        //  $this->expandedOptions - commandline provided options, merged with defaults
+        //  $this->topDir - top Moodle directory
+        //  $this->arguments[0] - first argument passed
+        //  $this->pluginInfo - array with information about the current plugin (based on cwd), keys:'type','name','dir'
+        //  $this->verbose - if set to true, then "moosh -v" was run - add more verbose / debug information
+
+        $options = $this->expandedOptions;
+
+        $cronlockfactory = \core\lock\lock_config::get_lock_factory('cron');
+        $cronlockfactory = get_class($cronlockfactory);
+        echo "Cron lock factory: $cronlockfactory\n";
+        
+        //  Example: dbname:mdl__cron_
+        $prefix = $CFG->dbname . ':' . $CFG->prefix . '_cron_';
+
+        // Get the list of all tasks
+        $tasks = \core\task\manager::get_all_scheduled_tasks();
+        foreach ($tasks as $task) {
+            $resourcekey = sha1($prefix . '\\' . get_class($task));
+
+            $result = $DB->get_record_sql('SELECT IS_USED_LOCK(:resourcekey) AS process', 
+                    ['resourcekey' => $resourcekey]);
+
+            if ($result->process) {
+                echo "[$resourcekey] " .  '\\' . get_class($task) . " locked by MySQL process {$result->process}\n";
+            } else if ($options['all'])  {
+                echo "[$resourcekey] " .  '\\' . get_class($task) . " not locked\n";
+            }
+        }
+    }
 }
