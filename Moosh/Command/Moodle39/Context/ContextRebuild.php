1fcc7e87d48803a14b22ea479baf73940f678f3f
Jakub Kleban <jakub.kleban@enovation.ie>
2021-08-02 12:13:27 +0200

    context-rebuild cleanup and simplify

--- Moosh/Command/Moodle39/Context/ContextRebuild.php
+++ Moosh/Command/Moodle39/Context/ContextRebuild.php
@@ -27,87 +27,46 @@ class ContextRebuild extends MooshCommand {
         global $CFG, $DB;
         require_once($CFG->dirroot.'/lib/accesslib.php');
 
-
         set_debugging(DEBUG_DEVELOPER, true);
-
-        $execute = '\core\task\context_cleanup_task';
-        if (!$task = \core\task\manager::get_scheduled_task($execute)) {
-            mtrace("Task '$execute' not found");
-            exit(1);
-        }
-
-        if (moodle_needs_upgrading()) {
-            mtrace("Moodle upgrade pending, cannot execute tasks.");
-            exit(1);
-        }
 
         // Increase memory limit.
         raise_memory_limit(MEMORY_EXTRA);
 
-        // Emulate normal session - we use admin account by default.
-        cron_setup_user();
-
         $predbqueries = $DB->perf_get_queries();
         $pretime = microtime(true);
 
-        \core\task\logmanager::start_logging($task);
-        $fullname = $task->get_name() . ' (' . get_class($task) . ')';
-        mtrace('Execute scheduled task: ' . $fullname);
-        // NOTE: it would be tricky to move this code to \core\task\manager class,
-        //       because we want to do detailed error reporting.
-        $cronlockfactory = \core\lock\lock_config::get_lock_factory('cron');
-        if (!$cronlock = $cronlockfactory->get_lock('core_cron', 10)) {
-            mtrace('Cannot obtain cron lock');
-            exit(129);
-        }
-        if (!$lock = $cronlockfactory->get_lock('\\' . get_class($task), 10)) {
-            $cronlock->release();
-            mtrace('Cannot obtain task lock');
-            exit(130);
-        }
-
-        $task->set_lock($lock);
-        if (!$task->is_blocking()) {
-            $cronlock->release();
-        } else {
-            $task->set_cron_lock($cronlock);
-        }
+        $fullname = 'Cleanup contexts (core\task\context_cleanup_task)';
+        echo "Execute scheduled task: $fullname\n";
 
         try {
-            get_mailer('buffer');
             self::fake_execute_with_true(); // Here was $task->execute();
             if (isset($predbqueries)) {
-                mtrace("... used " . ($DB->perf_get_queries() - $predbqueries) . " dbqueries");
-                mtrace("... used " . (microtime(1) - $pretime) . " seconds");
+                echo "... used " . ($DB->perf_get_queries() - $predbqueries) . " dbqueries\n";
+                echo "... used " . (microtime(1) - $pretime) . " seconds\n";
             }
-            mtrace('Scheduled task complete: ' . $fullname);
-            \core\task\manager::scheduled_task_complete($task);
-            get_mailer('close');
+            echo "Scheduled task complete: $fullname\n";
             exit(0);
         } catch (Exception $e) {
             if ($DB->is_transaction_started()) {
                 $DB->force_transaction_rollback();
             }
-            mtrace("... used " . ($DB->perf_get_queries() - $predbqueries) . " dbqueries");
-            mtrace("... used " . (microtime(true) - $pretime) . " seconds");
-            mtrace('Scheduled task failed: ' . $fullname . ',' . $e->getMessage());
+            echo "... used " . ($DB->perf_get_queries() - $predbqueries) . " dbqueries\n";
+            echo "... used " . (microtime(true) - $pretime) . " seconds\n";
+            echo 'Scheduled task failed: ' . $fullname . ',' . $e->getMessage() . PHP_EOL;
             if ($CFG->debugdeveloper) {
                 if (!empty($e->debuginfo)) {
-                    mtrace("Debug info:");
-                    mtrace($e->debuginfo);
+                    echo "Debug info:\n $e->debuginfo";
                 }
-                mtrace("Backtrace:");
-                mtrace(format_backtrace($e->getTrace(), true));
+                echo "Backtrace:\n";
+                echo format_backtrace($e->getTrace(), true);
             }
-            \core\task\manager::scheduled_task_failed($task);
-            get_mailer('close');
             exit(1);
         }
     }
 
     private function fake_execute_with_true(){
         \context_helper::cleanup_instances();
-        mtrace(' Cleaned up context instances');
+        echo " Cleaned up context instances\n";
         \context_helper::build_all_paths(true);
     }
 }
