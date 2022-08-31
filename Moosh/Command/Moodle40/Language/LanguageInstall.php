67ee5f803eec4106b1c2f1ada437aa684e7a7471
Tomasz Muras <nexor1984@gmail.com>
2022-08-31 20:38:11 +0200

    New commands: questionbank-import and language-install

---
+++ Moosh/Command/Moodle40/Language/LanguageInstall.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * moosh - Moodle Shell
+ *
+ * @copyright  2016 onwards Tomasz Muras
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace Moosh\Command\Moodle40\Language;
+
+use Moosh\MooshCommand;
+use \tool_langimport\controller;
+
+class LanguageInstall extends MooshCommand
+{
+    public function __construct()
+    {
+        parent::__construct('install', 'language');
+
+        $this->addArgument('lang');
+
+        //$this->addOption('t|test', 'option with no value');
+        //$this->addOption('o|option:', 'option with value and default', 'default');
+
+    }
+
+    public function execute()
+    {
+        // Some variables you may want to use
+        //  $this->cwd - the directory where moosh command was executed
+        //  $this->mooshDir - moosh installation directory
+        //  $this->expandedOptions - commandline provided options, merged with defaults
+        //  $this->topDir - top Moodle directory
+        //  $this->arguments[0] - first argument passed
+        //  $this->pluginInfo - array with information about the current plugin (based on cwd), keys:'type','name','dir'
+        //  $this->verbose - if set to true, then "moosh -v" was run - add more verbose / debug information
+
+        $lang = $this->arguments[0];
+
+        $controller = new controller();
+        $controller->install_languagepacks($lang);
+
+        /* if verbose mode was requested, show some more information/debug messages
+        if($this->verbose) {
+            echo "Say what you're doing now";
+        }
+        */
+    }
+}
