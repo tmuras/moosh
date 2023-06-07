031b871bb782585ffc8cbcf417fb51c48dc0a06e
Tomek Muras <tomek.muras@enovation.ie>
2023-06-07 16:39:17 +0200

    image generation wip

--- Moosh/Command/Moodle41/Course/CourseImageGenerate.php
+++ Moosh/Command/Moodle41/Course/CourseImageGenerate.php
@@ -8,6 +8,7 @@
 
 namespace Moosh\Command\Moodle41\Course;
 
+use local_ai_connector\ai\ai;
 use Moosh\MooshCommand;
 use Moosh\MoodleMetaData;
 use curl;
@@ -28,7 +29,7 @@ class CourseImageGenerate extends MooshCommand
 
     public function execute()
     {
-        global $CFG, $DB;
+        global $CFG, $DB, $USER;
         //some variables you may want to use
         //$this->cwd - the directory where moosh command was executed
         //$this->mooshDir - moosh installation directory
@@ -44,18 +45,23 @@ class CourseImageGenerate extends MooshCommand
 
         // extract image file from course image
         $course = $DB->get_record('course', array('id' => $courseid));
-        if(!$course) {
+        if (!$course) {
             cli_error('Invalid course id: ' . $courseid);
         }
         $coursecontext = \context_course::instance($course->id);
         $courseimage = $DB->get_record_sql("SELECT * FROM {files} WHERE filename <> ? AND contextid = ? AND 
 component= 'course' AND filearea = 'overviewfiles' AND 'itemid' = 0 LIMIT 1", array('.', $coursecontext->id));
-        if(!$courseimage) {
+        if (!$courseimage) {
             cli_error('No course image found for course id: ' . $courseid);
         }
-        $courseimagepath = $CFG->dataroot . '/filedir/' . substr($courseimage->contenthash, 0, 2) . '/' . substr($courseimage->contenthash,2, 2) . '/' . $courseimage->contenthash;
-        if(!file_exists($courseimagepath)) {
+        $courseimagepath = $CFG->dataroot . '/filedir/' . substr($courseimage->contenthash, 0, 2) . '/' . substr($courseimage->contenthash, 2, 2) . '/' . $courseimage->contenthash;
+        if (!file_exists($courseimagepath)) {
             cli_error('Course image file not found: ' . $courseimagepath);
+        }
+
+        $tempdir = $CFG->tempdir . '/moosh/';
+        if (!file_exists($tempdir)) {
+            mkdir($tempdir);
         }
 
         $image = new \Imagick($courseimagepath);
@@ -64,75 +70,85 @@ component= 'course' AND filearea = 'overviewfiles' AND 'itemid' = 0 LIMIT 1", ar
         $width = $image->getImageWidth();
         $size = min($width, $height);
         $image->cropThumbnailImage($size, $size);
+
+        unlink($tempdir . '/mainfile.png');
         // Save the image.
-        $image->writeImage($CFG->dataroot . '/temp/new.png');
+        $image->writeImage($tempdir . '/mainfile.png');
 
         $headers = [
             "Authorization: Bearer xxx",
             "Content-Type: multipart/form-data;charset=utf-8"
         ];
 
-        $curl = new \curl();
-        $options = [
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_HTTPHEADER => $headers,
-            CURLOPT_POSTFIELDS     => [
-                'prompt' => 'circle, square, triangle',
-                'size' => "256x256" ,
-//                'image' => new \CURLFile($courseimagepath),
-                'image' => curl_file_create($CFG->dataroot . '/temp/new.png'),
-            ]
-        ];
-        $curl = curl_init('https://api.openai.com/v1/images/edits');
+        // Get all the sections of Moodle course
+        $sections = $DB->get_records('course_sections', array('course' => $courseid), 'section ASC');
+        foreach ($sections as $section) {
+            $description = strip_tags($section->summary);
 
-        curl_setopt_array($curl, $options);
-        $response = curl_exec($curl);
+            echo "Processing section " . $section->section . ":\n";
 
-        $info           = curl_getinfo($curl);
-        $this->curlInfo = $info;
-print_r($response);
-var_dump($info);
-        curl_close($curl);
-die('ok');
-//        $data = [
-//            'prompt' => 'circle, square, triangle',
-//            'size' => "256x256" ,
-//            'image' => file_get_contents('/home/tmuras/Projects/2023_ai_image_generation/frog41t_ms.png'),
-//        ];
-        //var_dump(json_encode($data));
-        $response = $curl->post('https://api.openai.com/v1/images/edits', '', $options);
-        if (json_decode($response) == null) {
-            return ['curl_error' => $response];
+            $ask = "You are now a DALLE-2 prompt generation tool that will generate a suitable prompt for me to generate a picture 
+            based on the elearning course description, generate a prompt that gives the DALLE-2 AI text to generate a picture model,
+            please narrate in English. The elearning course description is:\n";
+            $ask .= '"' . $description . '"' . "\n";
+
+            $ai = new ai();
+            echo "Asking ChatGPT for:\n";
+            echo $ask . "\n";
+
+            $response = $ai->prompt_completion($ask);
+            $prompt = $response['content'];
+
+            echo "Prompt received:\n";
+            echo $prompt . "\n";
+
+            $options = [
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_HTTPHEADER => $headers,
+                CURLOPT_POSTFIELDS => [
+                    'prompt' => $prompt,
+                    'size' => "256x256",
+                    'image' => curl_file_create($tempdir . '/mainfile.png'),
+                ]
+            ];
+            $curl = curl_init('https://api.openai.com/v1/images/edits');
+
+            curl_setopt_array($curl, $options);
+            $response = curl_exec($curl);
+            $response = json_decode($response, true);
+            $info = curl_getinfo($curl);
+//            print_r($response);
+//            print_r($info);
+
+            // Download generated file into temp using Moodle function
+            $file = file_get_contents($response['data'][0]['url']);
+            $filename = 'dalle_' . $courseid . '_' . time() . '.png';
+            $temppath = $tempdir . '/' . $filename;
+            echo "Saving new image as $temppath\n";
+            file_put_contents($temppath, $file);
+
+            // Add image to Moodle course files
+            $filerecord = new \stdClass();
+            $filerecord->contextid = $coursecontext->id;
+            $filerecord->component = 'course';
+            $filerecord->filearea = 'section';
+            $filerecord->itemid = $section->id;
+            $filerecord->filepath = '/';
+            $filerecord->filename = $filename;
+            $filerecord->userid = $USER->id;
+            $filerecord->mimetype = 'image/png';
+
+            $fs = get_file_storage();
+            $fs->create_file_from_pathname($filerecord, $temppath);
+//            unlink($temppath);
+
+            $section->summary = '<p dir="ltr" style="text-align: left;"><img src="@@PLUGINFILE@@/'. $filename . '" alt="AI Generated" class="img-fluid atto_image_button_text-bottom" width="256" height="256"><br></p>' .
+                $section->summary;
+
+            // Inject into course section
+            $DB->update_record('course_sections', $section);
         }
-        var_dump(json_decode($response, true));
 
-        die();
-        $ai = new \local_ai_connector\ai\ai();
-        // Generate any image.
-        $url = $ai->prompt_dalle('Imagine a vibrant and engaging educational cover image for an online mathematics course specifically designed for highly talented children. The course ends with a focus on integrals, indicating a high level of complexity. This specific chapter is about Fractions, which includes the following subtopics:
-- Ordinary Fractions
-- Comparing Fractions
-- Addition and Subtraction of Fractions
-- Multiplying Fractions by Natural Numbers.
-    The image should be colorful, fun, and stimulate curiosity while still being clearly relevant to the topic. It might include various types of fractions and their visual representations, maybe as part of some exciting mathematical landscape or adventure. Also, consider including some elements related to integrals to reflect the overall course theme. Remember, this is for exceptionally talented children, so the image should not be overly simplified.
-    The image should have no text, no written words."
-');
-        var_dump($url);
-
-        // Check if URL is valid.
-        if(strpos($url, 'https://') !== 0) {
-            cli_error('Invalid response from dalle: ' . $url);
-        }
-
-        // Fetch the image into Moodle's temp directory.
-        $tempdir = $CFG->tempdir . '/moosh/';
-        if(!file_exists($tempdir)) {
-            mkdir($tempdir);
-        }
-        $temppath = $tempdir . '/dalle_' .  $courseid . '_' . time() . '.png';
-        file_put_contents($temppath, file_get_contents($url));
-
-        // Add it into course files.
 
 
     }
