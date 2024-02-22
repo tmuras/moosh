2c9e33d7b2cc62a774212ac0618b018b41cde675
odziminski <o.dziminski@gmail.com>
2024-02-22 16:40:10 +0100

    add QuestionDelete command

---
+++ Moosh/Command/Moodle41/Question/QuestionDelete.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * moosh - Moodle Shell
+ *
+ * @copyright  2012 onwards Tomasz Muras
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace Moosh\Command\Moodle41\Question;
+
+use Moosh\MooshCommand;
+
+class QuestionDelete extends MooshCommand
+{
+    public function __construct()
+    {
+        parent::__construct('delete', 'question');
+        $this->addArgument('questionid');
+    }
+
+    public function execute()
+    {
+        global $DB, $CFG;
+        $questionid = $this->arguments[0];
+
+        require_once($CFG->dirroot . '/question/engine/lib.php');
+        require_once($CFG->dirroot . '/question/type/questiontypebase.php');
+
+        $question = $DB->get_record('question', ['id' => $questionid]);
+
+        if (!$question) {
+            mtrace("[ID:$questionid] No question found with id");
+            return;
+        }
+
+        $questionsToCheck = [$question->id];
+
+        if ($question->parent) {
+            $questionsToCheck[] = $question->parent;
+        }
+        if (questions_in_use($questionsToCheck)) {
+            mtrace("[ID:$questionid] Question in use in activities - can not delete");
+            return;
+        }
+        question_delete_question($question->id);
+
+    }
+}
+
