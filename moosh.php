16f5bcf18b163f8ab9238a7de5d517bd0f4813a7
Tomasz Muras <nexor1984@gmail.com>
2024-02-23 16:29:58 +0100

    version bump

--- moosh.php
+++ moosh.php
@@ -32,7 +32,7 @@ require_once $moosh_dir . '/includes/default_options.php';
 use GetOptionKit\ContinuousOptionParser;
 use GetOptionKit\OptionCollection;
 
-define('MOOSH_VERSION', '1.17');
+define('MOOSH_VERSION', '1.18');
 define('MOODLE_INTERNAL', true);
 
 $appspecs = new OptionCollection;
