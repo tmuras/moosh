33e9bc41239dc79a43aa14592703c9f57083bf69
Tomek Muras <tomek.muras@enovation.ie>
2024-05-01 10:03:38 +0200

    Version bump to 1.21

--- moosh.php
+++ moosh.php
@@ -32,7 +32,7 @@ require_once $moosh_dir . '/includes/default_options.php';
 use GetOptionKit\ContinuousOptionParser;
 use GetOptionKit\OptionCollection;
 
-define('MOOSH_VERSION', '1.20');
+define('MOOSH_VERSION', '1.21');
 define('MOODLE_INTERNAL', true);
 
 $appspecs = new OptionCollection;
