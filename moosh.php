dfae8f2a7d7ca7e97f8f6a1fbc5bb67d2c360087
Tomasz Muras <nexor1984@gmail.com>
2025-04-14 10:24:54 +0200

    Version bump to 1.29

--- moosh.php
+++ moosh.php
@@ -32,7 +32,7 @@ require_once $moosh_dir . '/includes/default_options.php';
 use GetOptionKit\ContinuousOptionParser;
 use GetOptionKit\OptionCollection;
 
-define('MOOSH_VERSION', '1.27');
+define('MOOSH_VERSION', '1.29');
 define('MOODLE_INTERNAL', true);
 
 // suppressing warnings for php > 8
