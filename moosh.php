e4f43fd185036cf2a47e831a785486241ad77f8b
Tomasz Muras <nexor1984@gmail.com>
2025-12-16 17:12:46 +0100

    Version bump to 1.41

--- moosh.php
+++ moosh.php
@@ -32,7 +32,7 @@ require_once $moosh_dir . '/includes/default_options.php';
 use GetOptionKit\ContinuousOptionParser;
 use GetOptionKit\OptionCollection;
 
-define('MOOSH_VERSION', '1.40');
+define('MOOSH_VERSION', '1.41');
 define('MOODLE_INTERNAL', true);
 
 // suppressing warnings for php > 8
